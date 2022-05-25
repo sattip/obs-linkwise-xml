5860ef99b9dd284ec65f7981f3297a1673826c94
manojkcoder <57411378+manojkcoder@users.noreply.github.com>
2022-05-26 00:14:49 +0530

    Update cron.php

--- cron.php
+++ cron.php
@@ -4,6 +4,6 @@ $pagePath = explode('/wp-content/',dirname(__FILE__));
 include_once($pagePath[0].'/wp-load.php');
 require_once(dirname(__FILE__).'/obs-linkwise-xml.php');
 
-$$obs_plugin_obj->init();
-$$obs_plugin_obj->mode = 'bulk';
-$$obs_plugin_obj->obs_woo_export_schedule_cron();
+$obs_plugin_obj->init();
+$obs_plugin_obj->mode = 'bulk';
+$obs_plugin_obj->obs_woo_export_schedule_cron();
